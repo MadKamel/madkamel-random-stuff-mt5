0fd19feade1dbdb473f0106c110b699de2ede60f
Eric Rustrum <rustrumek@gmail.com>
2021-07-31 14:42:25 -0400

    Added ingots.
    In particular strange metal ingots. And other recipes.

---
+++ crafting.lua
@@ -0,0 +1,79 @@
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_blue 16",
+    recipe = {
+        {"default:steelblock", "default:steelblock", "default:steelblock"},
+        {"default:steelblock", "default:diamondblock", "default:steelblock"},
+        {"default:mese", "default:mese", "default:mese"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_pink 16",
+    recipe = {
+        {"default:mese", "default:mese", "default:mese"},
+        {"default:steelblock", "default:diamondblock", "default:steelblock"},
+        {"default:steelblock", "default:steelblock", "default:steelblock"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_blue",
+    recipe = {
+        {"madkamel_random_stuff:unknown_metal_ingot_pink"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_pink",
+    recipe = {
+        {"madkamel_random_stuff:unknown_metal_ingot_blue"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_blue 9",
+    recipe = {
+        {"madkamel_random_stuff:blue_unknown_metal"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_ingot_pink 9",
+    recipe = {
+        {"madkamel_random_stuff:pink_unknown_metal"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:blue_unknown_metal",
+    recipe = {
+        {"madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue"},
+        {"madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue"},
+        {"madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue", "madkamel_random_stuff:unknown_metal_ingot_blue"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:pink_unknown_metal",
+    recipe = {
+        {"madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink"},
+        {"madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink"},
+        {"madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink", "madkamel_random_stuff:unknown_metal_ingot_pink"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal 2",
+    recipe = {
+        {"madkamel_random_stuff:pink_unknown_metal"},
+	{"madkamel_random_stuff:blue_unknown_metal"}
+    }
+})
+
+minetest.register_craft({
+    output = "madkamel_random_stuff:unknown_metal_invert 2",
+    recipe = {
+        {"madkamel_random_stuff:blue_unknown_metal"},
+	{"madkamel_random_stuff:pink_unknown_metal"}
+    }
+})
---
+++ craftitem.lua
@@ -0,0 +1,9 @@
+minetest.register_craftitem("madkamel_random_stuff:unknown_metal_ingot_blue", {
+    description = "Blue Strange Metal Ingot",
+    inventory_image = "unknown_ingot_blue.png"
+})
+
+minetest.register_craftitem("madkamel_random_stuff:unknown_metal_ingot_pink", {
+    description = "Pink Strange Metal Ingot",
+    inventory_image = "unknown_ingot_pink.png"
+})
