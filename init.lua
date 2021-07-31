1eac5aa27a9613d9f0140b9bbd553979d1d3398b
Eric Rustrum <rustrumek@gmail.com>
2021-07-30 22:14:00 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+local default_path = minetest.get_modpath("madkamel_random_stuff")
+
+dofile(default_path.."/nodes.lua")
+dofile(default_path.."/crafting.lua")
+dofile(default_path.."/abm.lua")
+dofile(default_path.."/liquid.lua")
---
+++ nodes.lua
@@ -0,0 +1,5 @@
+minetest.register_node("madkamel_random_stuff:unknown_metal", {
+	description = "Strange Metal Block",
+	tiles = {"unknown_metal_top.png", "unknown_metal_bottom.png", "unknown_metal_side.png"},
+	groups = {cracky = 1}
+})
