6f8d70856b96a5cbce803c15c863abf3af4f1b4a
Eric Rustrum <rustrumek@gmail.com>
2021-07-30 22:23:25 -0400

    Set up initial nodes

--- nodes.lua
+++ nodes.lua
@@ -3,3 +3,22 @@ minetest.register_node("madkamel_random_stuff:unknown_metal", {
 	tiles = {"unknown_metal_top.png", "unknown_metal_bottom.png", "unknown_metal_side.png"},
 	groups = {cracky = 1}
 })
+
+minetest.register_node("madkamel_random_stuff:unknown_metal_invert", {
+	description = "Inverted Strange Metal Block",
+	tiles = {"unknown_metal_bottom.png", "unknown_metal_top.png", "unknown_metal_side_invert.png"},
+	groups = {cracky = 1}
+})
+
+minetest.register_node("madkamel_random_stuff:pink_unknown_metal", {
+	description = "Pink Strange Metal Block",
+	tiles = {"unknown_metal_pink.png"},
+	groups = {cracky = 1}
+})
+
+
+minetest.register_node("madkamel_random_stuff:blue_unknown_metal", {
+	description = "Blue Strange Metal Block",
+	tiles = {"unknown_metal_blue.png"},
+	groups = {cracky = 1}
+})
