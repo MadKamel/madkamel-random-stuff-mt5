5d3afcde80efd803ed4485a4d4237b55cb23db5c
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 14:25:12 -0700

    Added strange metal ores

--- nodes.lua
+++ nodes.lua
@@ -23,6 +23,21 @@ minetest.register_node("madkamel_random_stuff:blue_unknown_metal", {
 	groups = {cracky = 1}
 })
 
+minetest.register_node("madkamel_random_stuff:stone_with_blue_unknown_metal", {
+	description = "Blue Strange Metal Ore",
+	tiles = {"default_stone.png^unknown_metal_blue_mineral.png"},
+	groups = {cracky = 1},
+	drops = {"madkamel_random_stuff:unknown_metal_lump_blue"}
+})
+
+
+minetest.register_node("madkamel_random_stuff:stone_with_pink_unknown_metal", {
+	description = "Pink Strange Metal Ore",
+	tiles = {"default_stone.png^unknown_metal_pink_mineral.png"},
+	groups = {cracky = 1},
+	drops = {"madkamel_random_stuff:unknown_metal_lump_pink"}
+})
+
 minetest.register_node("madkamel_random_stuff:blue_unknown_glass", {
 	description = "Blue Strange Glass",
 	drawtype = "glasslike_framed_optional",
