c1bebbae2547b4256478646d24688c0747971e1a
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 14:19:01 -0700

    Added cooking recipe for unknown metal ingots.

--- crafting.lua
+++ crafting.lua
@@ -1,22 +1,4 @@
 minetest.register_craft({
-    output = "madkamel_random_stuff:unknown_metal_ingot_blue 16",
-    recipe = {
-        {"default:steelblock", "default:steelblock", "default:steelblock"},
-        {"default:steelblock", "default:diamondblock", "default:steelblock"},
-        {"default:mese", "default:mese", "default:mese"}
-    }
-})
-
-minetest.register_craft({
-    output = "madkamel_random_stuff:unknown_metal_ingot_pink 16",
-    recipe = {
-        {"default:mese", "default:mese", "default:mese"},
-        {"default:steelblock", "default:diamondblock", "default:steelblock"},
-        {"default:steelblock", "default:steelblock", "default:steelblock"}
-    }
-})
-
-minetest.register_craft({
     output = "madkamel_random_stuff:unknown_metal_ingot_blue",
     recipe = {
         {"madkamel_random_stuff:unknown_metal_ingot_pink"}
@@ -89,5 +71,19 @@ minetest.register_craft({
     type = "cooking",
     output = "madkamel_random_stuff:pink_unknown_glass",
     recipe = "madkamel_random_stuff:unknown_metal_ingot_pink",
+    cooktime = 4,
+})
+
+minetest.register_craft({
+    type = "cooking",
+    output = "madkamel_random_stuff:unknown_metal_ingot_blue",
+    recipe = "madkamel_random_stuff:unknown_metal_lump_blue",
+    cooktime = 4,
+})
+
+minetest.register_craft({
+    type = "cooking",
+    output = "madkamel_random_stuff:unknown_metal_ingot_pink",
+    recipe = "madkamel_random_stuff:unknown_metal_lump_pink",
     cooktime = 4,
 })
