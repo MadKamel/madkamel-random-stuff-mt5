65c9a594299af0de70b958ca6ddfc494929802e8
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 14:27:50 -0700

    Added strange metal ores

---
+++ ore.lua
@@ -0,0 +1,21 @@
+minetest.register_ore({
+    ore_type = "scatter",
+    ore = "madkamel_random_stuff:stone_with_pink_unknown_metal",
+    wherein = "default:stone",
+    clust_scarcity = 8 * 8 * 8,
+    clust_num_ores = 99,
+    clust_size = 4,
+    y_min = -31000,
+    y_max = 31000
+})
+
+minetest.register_ore({
+    ore_type = "scatter",
+    ore = "madkamel_random_stuff:stone_with_blue_unknown_metal",
+    wherein = "default:stone",
+    clust_scarcity = 8 * 8 * 8,
+    clust_num_ores = 99,
+    clust_size = 4,
+    y_min = -31000,
+    y_max = 31000
+})
