e3aea4bba0a66c252efd69639174dbf98048a629
Eric Rustrum <rustrumek@gmail.com>
2021-08-18 14:17:07 -0700

    Added strange metal lumps

--- craftitem.lua
+++ craftitem.lua
@@ -7,3 +7,13 @@ minetest.register_craftitem("madkamel_random_stuff:unknown_metal_ingot_pink", {
     description = "Pink Strange Metal Ingot",
     inventory_image = "unknown_ingot_pink.png"
 })
+
+minetest.register_craftitem("madkamel_random_stuff:unknown_metal_lump_blue", {
+    description = "Blue Strange Metal Lump",
+    inventory_image = "unknown_lump_blue.png"
+})
+
+minetest.register_craftitem("madkamel_random_stuff:unknown_metal_lump_pink", {
+    description = "Pink Strange Metal Lump",
+    inventory_image = "unknown_lump_pink.png"
+})
